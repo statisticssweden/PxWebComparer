42c74ce14494c176d70b7ad319deb60b6c4271f0
Kent Mossbäck <kent.mossback@gmail.com>
2020-06-15 12:35:45 +0200

    Add files via upload

---
+++ PxWebCombarer/Services/ISavedQueryService.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Net.Http;
+
+namespace PxWebComparer.Services
+{
+     public interface ISavedQueryService
+     {
+         string GetSavedQuery(string url);
+         void SaveToFile(string content, string fileName, string contentType, string path);
+     }
+}
---
+++ PxWebCombarer/Services/SavedQueryService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace PxWebComparer.Services
+{
+    public class SavedQueryService : ISavedQueryService
+    {
+        public string GetSavedQuery(string url)
+        {
+            //url = "http://pxwebdemo.scb.se/sq/2b33e238-8e7e-44ee-bbdf-21a007a4069a.csv";
+            string resultString = string.Empty;
+            //.csv
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            //request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        resultString = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine("\r\nWebException Raised. The following error occurred : {0}", e.Status);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThe following Exception was raised : {0}", e.Message);
+            }
+            return resultString;
+        }
+        
+        public void SaveToFile(string content, string fileName, string savedQueryFormat, string path)
+        {
+            var file = new FileInfo($@"{path}\{fileName}.{savedQueryFormat}");
+            file.Directory.Create();
+            
+            File.WriteAllText(
+                Path.Combine(path, $"{fileName}.{savedQueryFormat}"),
+                content,
+                Encoding.UTF8);
+        }
+    }   
+}
