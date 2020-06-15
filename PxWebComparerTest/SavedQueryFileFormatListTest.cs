9998d2b8ada696946e522bb540c6980a2cffd25f
Kent Mossbäck <kent.mossback@gmail.com>
2020-06-15 12:37:37 +0200

    Add files via upload

---
+++ PxWebComparerTest/SavedQueryFileFormatListTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PxWebComparer.Repo;
+
+namespace PxWebComparerTest
+{
+    [TestClass]
+    public class SavedQueryFileFormatListTest
+    {
+        [TestMethod]
+        public void ShouldReturnSavedQueryList()
+        {
+            var repo = new SavedQueryFileFormatRepo();
+            var path = @"C:\Temp\PxWebComparerTest\ThreeQueryFile.txt";
+            var result = repo.GetSavedQueryFileFormat(path);
+
+        }
+
+
+    }
+}
