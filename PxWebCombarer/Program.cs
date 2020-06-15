b800cc5e946421bb0c1edf6699679de94fcfede4
Kent Mossbäck <kent.mossback@gmail.com>
2020-06-15 12:28:46 +0200

    Add files via upload

---
+++ PxWebCombarer/Program.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PxWebComparer.Business;
+using PxWebComparer.Services;
+
+namespace PxApiComparer
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var handler = new CompareHandler();
+
+            handler.Compare();
+
+            //var savedQueryService = new SavedQueryService();
+
+            //savedQueryService.GetSavedQuery(String.Empty);
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+        //static void ReadSetting(string key)
+        //{
+        //    try
+        //    {
+        //        var appSettings = ConfigurationManager.AppSettings;
+        //        string result = appSettings[key] ?? "Not Found";
+        //        Console.WriteLine(result);
+        //    }
+        //    catch (ConfigurationErrorsException)
+        //    {
+        //        Console.WriteLine("Error reading app settings");
+        //    }
+        //}
+
+
+        //static void ReadAllSettings()
+        //{
+        //    try
+        //    {
+        //        var appSettings = ConfigurationManager.AppSettings;
+
+        //        if (appSettings.Count == 0)
+        //        {
+        //            Console.WriteLine("AppSettings is empty.");
+        //        }
+        //        else
+        //        {
+        //            foreach (var key in appSettings.AllKeys)
+        //            {
+        //                Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+        //            }
+        //        }
+        //    }
+        //    catch (ConfigurationErrorsException)
+        //    {
+        //        Console.WriteLine("Error reading app settings");
+        //    }
+        //}
+
+        //static void ReadSetting(string key)
+        //{
+        //    try
+        //    {
+        //        var appSettings = ConfigurationManager.AppSettings;
+        //        string result = appSettings[key] ?? "Not Found";
+        //        Console.WriteLine(result);
+        //    }
+        //    catch (ConfigurationErrorsException)
+        //    {
+        //        Console.WriteLine("Error reading app settings");
+        //    }
+        //}
+
+        //static void AddUpdateAppSettings(string key, string value)
+        //{
+        //    try
+        //    {
+        //        var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        //        var settings = configFile.AppSettings.Settings;
+        //        if (settings[key] == null)
+        //        {
+        //            settings.Add(key, value);
+        //        }
+        //        else
+        //        {
+        //            settings[key].Value = value;
+        //        }
+        //        configFile.Save(ConfigurationSaveMode.Modified);
+        //        ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        //    }
+        //    catch (ConfigurationErrorsException)
+        //    {
+        //        Console.WriteLine("Error writing app settings");
+        //    }
+        //}
+
+
+    }
+}
