421fadfec9dd38055b67cf203ebe8f130676ad35
Kent Mossbäck <kent.mossback@gmail.com>
2020-06-15 12:33:25 +0200

    Add files via upload

---
+++ PxWebCombarer/Model/Enums/OutputFormat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PxWebComparer.Model.Enums
+{
+    public enum OutputFormat
+    {
+         px
+        ,xlsx
+        ,xlsx_doublecolumn
+        ,csv
+        ,csv_tab
+        ,csv_tabhead
+        ,csv_comma
+        ,csv_commahead
+        ,csv_space
+        ,csv_spacehead
+        ,csv_semicolon
+        ,csv_semicololhead
+        ,json_stat
+        ,html5_table
+        ,relational_table
+        ,json
+    }
+}
